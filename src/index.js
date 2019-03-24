a3ef744dbf752f2e4a82fe1da8ca66926597ff94
Ilya Rozhkovsky <poiskull@gmail.com>
2019-03-24 20:25:07 +0300

    feat: add love triangles counter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+    let firstElement, secondElement, thirdElement;
+    let result = 0;
+
+    for (let i = 0; i < preferences.length; i++) {
+
+        firstElement = preferences[i];
+        secondElement = preferences[firstElement-1];
+        thirdElement = preferences[secondElement-1];
+
+        if ((thirdElement == i+1) && (firstElement < secondElement) && (thirdElement < secondElement))
+        {
+            result++;
+        }
+    }
+
+    return result;
 };
